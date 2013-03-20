6f97850d53c45ad5d7ff1ba77a724d0e136395d4
v pradeep kumar raju <vpradeep.raj@gmail.com>
2013-03-20 22:20:04 +0530

    Create oper.py

---
+++ oper.py
@@ -0,0 +1,11 @@
+# math operations
+
+def ope():
+    x=int(raw_input("enter num1:"))
+    y=int(raw_input("enter num2:"))
+    print "sum:",x+y
+    print "sub",x-y
+    print "multiply",x*y
+    print "division",x/y
+
+ope()
