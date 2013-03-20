e72accc3ad00f075698cac72294ff662c065e670
v pradeep kumar raju <vpradeep.raj@gmail.com>
2013-03-20 22:16:55 +0530

    Create maths_class.py

---
+++ maths_class.py
@@ -0,0 +1,11 @@
+# sum,sub of a class
+class maths:
+      def __init(self):
+          pass
+      def sum(self,a,b):
+          print a+b
+      def sub(self,a,b):
+          print a-b
+t=maths()
+t.sum(3,2)
+t.sub(3,2)
