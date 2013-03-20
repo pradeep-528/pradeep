99b564f71f3d336920c903181a18a7f98e3ddf9d
v pradeep kumar raju <vpradeep.raj@gmail.com>
2013-03-20 22:31:52 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# slicing
+
+l = [1,2,3,4,5,6,7]
+
+print l
+
+print "lst element:",l[0]
+
+print "last elemen:",l[-1]
+
+print "1 to 3:",l[0:3]
+
+print "reverse:",l[::-1]
+
+s='pradeep'
+
+print s
+
+print "reverse of a string:",s[::-1]
