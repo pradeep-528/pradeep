12d7221ed253826ed17d0e6e4b2441db11e2ecfa
v pradeep kumar raju <vpradeep.raj@gmail.com>
2013-03-20 22:12:54 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python
+# comparision of two values
+
+a=int(raw_input("enter a value:"))
+b=int(raw_input("enter b value:"))
+if a>b:
+   print "a is greater than b"
+elif b>a:
+   print "b is greater than a"
+elif a==b:
+   print "a is equal to b"
+
